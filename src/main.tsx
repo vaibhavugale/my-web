34c4aaaf418220e4dbc3046750ee19c3f590753e
vaibhavugale <ugalevaibhav00@gmail.com>
2026-04-16 15:15:09 +0530

    feat: integrate Microsoft Clarity for user behavior analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import clarity from '@microsoft/clarity'
 import './index.css'
 import App from './App.tsx'
+
+// TODO: Replace 'YOUR_PROJECT_ID' with your real Microsoft Clarity Project ID
+clarity.init('wci0l3750r');
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
